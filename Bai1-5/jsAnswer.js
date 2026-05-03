3e01bbc82a153eaa181bfde02ae4e2cbcfdb136a
Ha <25a1001d0126@students.hou.edu.vn>
2026-05-03 20:49:01 +0700

    Halia

--- Bai1-5/jsAnswer.js
+++ Bai1-5/jsAnswer.js
@@ -1,2 +1,39 @@
 //Tao cũng tạo .js này để chúng mày không cần làm :>>>
 //Write your answer below:
+// Bài 1
+/*
+let x, y, total;
+document.getElementById("submitButton").onclick = function() {
+  x = document.getElementById("x").value ;
+  y = document.getElementById("y").value ;
+  x= Number(x);
+  y= Number(y);
+  console.log(typeof x);
+  console.log(typeof y);
+  total= x+y;
+  document.getElementById("result").textContent = `Tổng: ${total}`;
+};
+*/
+// Bài 2
+/*
+document.getElementById("submitButton").onclick = function() {
+    document.getElementById("line").textContent = ` Tên: ${document.getElementById("name").value} ; Tuổi: ${document.getElementById("age").value} .`;
+}
+*/
+//Bài 3
+/*
+let x,y,z,result;
+document.getElementById("submitButton").onclick = function() {
+    x=document.getElementById("x").value;
+    y=document.getElementById("y").value;
+    z=document.getElementById("z").value;
+    x= Number(x);
+    y= Number(y);
+    z= Number(z);
+    result = (x+y+z)/(3**3);
+    result=String(result);
+    result+=1;
+    console.log(`Kết quả cần tìm: ${result}`);
+    document.getElementById("result").textContent = ` Kết quả cần tìm: ${typeof result}`;
+}
+*/
--- Bai6-10/jsAnswer.js
+++ Bai6-10/jsAnswer.js
@@ -1,2 +1,34 @@
 //Tao cũng tạo .js này để chúng mày không cần làm :>>>
 //Write your answer below:
+let count = 0;
+let radius1, radius2, radius3, radius4, radius5;
+submit = document.getElementById("submitButton");
+randomNum = document.getElementById("random");
+randomNum.onclick = function () {
+  let min = 1;
+  let max = 100;
+  document.getElementById("radius1").value = Math.trunc(Math.random() * max + min);
+  document.getElementById("radius2").value = Math.trunc(Math.random() * max + min);
+  document.getElementById("radius3").value = Math.trunc(Math.random() * max + min);
+  document.getElementById("radius4").value = Math.trunc(Math.random() * max + min);
+  document.getElementById("radius5").value = Math.trunc(Math.random() * max + min);
+};
+submit.onclick = function () {
+  radius1 = Number(document.getElementById("radius1").value);
+  radius2 = Number(document.getElementById("radius2").value);
+  radius3 = Number(document.getElementById("radius3").value);
+  radius4 = Number(document.getElementById("radius4").value);
+  radius5 = Number(document.getElementById("radius5").value);
+  if (Math.cos(Math.PI * radius1 ** 2) > 0) ++count;
+  if (Math.cos(Math.PI * radius2 ** 2) > 0) ++count;
+  if (Math.cos(Math.PI * radius3 ** 2) > 0) ++count;
+  if (Math.cos(Math.PI * radius4 ** 2) > 0) ++count;
+  if (Math.cos(Math.PI * radius5 ** 2) > 0) ++count;
+  document.getElementById("result1").textContent =
+    ` Số lượng các hình tròn có cos(diện tích) > 0 : ${count}`;
+  document.getElementById("result2").textContent =
+    ` Rmin: ${Math.min(radius1, radius2, radius3, radius4, radius5)} `;
+  console.log(
+    `${Math.floor(radius1 / (2 * Math.PI))}; ${Math.floor(radius2 / (2 * Math.PI))}; ${Math.floor(radius3 / (2 * Math.PI))}; ${Math.floor(radius4 / (2 * Math.PI))}; ${Math.floor(radius5 / (2 * Math.PI))}`);
+  count=0;
+};
